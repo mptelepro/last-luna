eea4902a177661f38cf8d21e101eb15eaac2e88d
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-27 15:59:42 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,6 @@
-owner_id = 1243703097
-bot_token = "16901971:AAFqdM_SQE1PB2P1xLr67k"
-bot_id = 1663901971
+owner_id = 1650092910
+bot_token = "1773078738:AAGEtkCal_daT2jxH5QXY7-Pn3jqMkIvkWo"
+bot_id = 1773078738
 
 # Leave it as it is
 ARQ_API_BASE_URL = "https://thearq.tech"
